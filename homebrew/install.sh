d37dc6028ab4f4cead141e677ed0fe4a91cd9cc6
pangratz <cmueller.418@gmail.com>
2019-04-25 12:31:12 +0200

    [homebrew] update path to installation script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,7 +9,7 @@
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" > /tmp/homebrew-install.log
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Install homebrew packages from se Brewfile
