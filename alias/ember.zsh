af4bf4aea7f8515d4bcac762c0937de6be868b58
Clemens Müller <cmueller.418@gmail.com>
2019-06-17 13:31:10 +0200

    [zsh-alias] update nombom
    We need to --force the cache clearing, otherwise we're running into an
    error since cache clearing is not safe by default.

--- alias/ember.zsh
+++ alias/ember.zsh
@@ -1,1 +1,1 @@
-alias nombom='npm cache clear && bower cache clean && rm -rf node_modules bower_components && npm install && bower install'
+alias nombom='npm cache clear --force && bower cache clean && rm -rf node_modules bower_components && npm install && bower install'
