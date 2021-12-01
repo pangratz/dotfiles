292122d5918165b72de6ed6937ac09dcfc08d55a
Clemens Müller <cmueller.418@gmail.com>
2021-12-01 09:32:39 +0100

    [node] add volta

--- node/path.zsh
+++ node/path.zsh
@@ -1,3 +1,6 @@
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
+
 # add node_modules to path
 export PATH=$PATH:/usr/local/lib/node_modules
 export PATH=$PATH:/usr/local/share/npm/bin
