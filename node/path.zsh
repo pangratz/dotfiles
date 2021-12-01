025808cf5bb490f171137f408be710872ebb07fb
Clemens Müller <cmueller.418@gmail.com>
2021-12-01 09:33:28 +0100

    [node] remove node and yarn from Brewfile
    volta is used now to manage node and yarn globally installed tools

--- node/path.zsh
+++ node/path.zsh
@@ -1,6 +1,2 @@
 export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
-
-# add node_modules to path
-export PATH=$PATH:/usr/local/lib/node_modules
-export PATH=$PATH:/usr/local/share/npm/bin
