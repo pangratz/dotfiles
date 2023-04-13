cc50442488535592d1e06e0f64797497f255cc65
Clemens M�ller <cmueller.418@gmail.com>
2023-04-13 10:13:28 +0200

    [zsh] Make pasting of URLs work with download_ functions
    Pasting a URL now properly escapes them automatically so we don't need
    to prefix with `noglob`
    
        download_video https://www.youtube.com/watch?v=dQw4w9WgXcQ

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -7,6 +7,12 @@ autoload -U $DOTFILES/functions/*(:t)
 # prevent "Fatal error: EMFILE: Too many opened files." error message
 ulimit -n 10000
 
+# prevent "no matches found" when url is pasted
+# https://superuser.com/a/1237124/128931
+autoload -Uz bracketed-paste-magic
+zle -N bracketed-paste bracketed-paste-magic
+autoload -Uz url-quote-magic
+zle -N self-insert url-quote-magic
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
